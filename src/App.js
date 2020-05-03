3c05a71aa40b2f2f000cc9e3b6b7137f5298289c
Ketki Panse <ketki.panse@shopify.com>
2020-05-03 12:54:29 -0400

    Making a card holder to put all cards in

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card'
+import CardHolder from './components/CardHolder'
 
 function App() {
-  let weather_report = {
-      day: 'Monday',
+  let weather_report = [
+      {day: 'Sunday',
       weather: 'Cloudy',
       max_temp: 20,
-      min_temp: 12
-    }
+      min_temp: 12},
+      {day: 'Monday',
+      weather: 'Cloudy',
+      max_temp: 20,
+      min_temp: 12},
+      {day: 'Tuesday',
+      weather: 'Cloudy',
+      max_temp: 20,
+      min_temp: 12},
+      {day: 'Wednesday',
+      weather: 'Cloudy',
+      max_temp: 20,
+      min_temp: 12},
+      {day: 'Thursday',
+      weather: 'Cloudy',
+      max_temp: 20,
+      min_temp: 12},
+      {day: 'Friday',
+      weather: 'Cloudy',
+      max_temp: 20,
+      min_temp: 12},
+      {day: 'Saturday',
+      weather: 'Cloudy',
+      max_temp: 20,
+      min_temp: 12}
+    ];
   return (
     <div className="App">
-      <Card weather_report={weather_report}/>
+      <CardHolder weather_report={weather_report}/>
     </div>
   );
 }
--- src/components/CardHolder/CardHolder.js
+++ src/components/CardHolder/CardHolder.js
@@ -1,7 +1,23 @@
 import React from "react";
+import './CardHolder.css'
+import Card from '../Card'
 
-const CardHolder = () => {
-  return <div />;
+const createTable = (props) => {
+      let days = []
+      //Inner loop to create children
+
+      for (let i = 0; i<props.weather_report.length; i++) {
+        days.push(<Card weather_report={props.weather_report[i]} key={i}/>);
+      }
+    return days;
+  }
+
+const CardHolder = (props) => {
+  return (
+    <section className='weather_inline'>
+      {createTable(props)}
+    </section>
+  );
 };
 
 export default CardHolder;
